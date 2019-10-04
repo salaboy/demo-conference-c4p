b8fee835d09b32c62efa8f70ec72c14e2ae28e44
salaboy <salaboy@gmail.com>
2019-10-04 11:17:56 +0100

    adding source for emitting events

--- src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
@@ -3,20 +3,23 @@ package com.salaboy.conferences.c4p;
 import com.salaboy.conferences.c4p.model.Proposal;
 import com.salaboy.conferences.c4p.model.ProposalDecision;
 import com.salaboy.conferences.c4p.model.ProposalStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @SpringBootApplication
 @RestController
-@EnableBinding(Processor.class)
+@EnableBinding(Source.class)
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -27,6 +30,9 @@ public class DemoApplication {
     private String version;
 
     private Set<Proposal> proposals = new HashSet<>();
+
+    @Autowired
+    private Source source;
 
     @GetMapping("/info")
     public String infoWithVersion() {
@@ -58,7 +64,7 @@ public class DemoApplication {
             Proposal proposal = proposalOptional.get();
             proposal.setRank(rank);
             proposal.setStatus(ProposalStatus.RANKED);
-            System.out.println(">  Proposal Ranked: " + proposal);
+            emitEvent(">  Proposal Ranked: " + proposal);
             proposals.add(proposal);
         }
     }
@@ -70,26 +76,21 @@ public class DemoApplication {
             Proposal proposal = proposalOptional.get();
             proposal.setApproved(decision.isApproved());
             proposal.setStatus(ProposalStatus.DECIDED);
-            System.out.println(">  Proposal Approved: " + proposal);
+            emitEvent(">  Proposal Approved: " + ((decision.isApproved()) ? "Approved" : "Rejected"));
             proposals.add(proposal);
+            emitEvent("> Notify Speaker via email: " + proposal.getEmail() + " -> " + ((decision.isApproved()) ? "Approved" : "Rejected"));
         }
 
-        System.out.println("> Notify Speaker");
 
-
-        if(decision.isApproved()) {
-            System.out.println("> Send request to Agenda");
+        if (decision.isApproved()) {
+            emitEvent("> Send request to publish talk to Agenda Service");
         }
 
-        // Call Agenda Service
-        // POST proposal data to AGENDA / for new item to be added in the agenda
     }
 
-
-    @StreamListener(Processor.INPUT)
-    @SendTo(Processor.OUTPUT)
-    public byte[] handle(byte[] in){
-        return new String(in).toUpperCase().getBytes();
+    private void emitEvent(String content) {
+        System.out.println(content);
+        source.output().send(MessageBuilder.createMessage(content, null));
     }
 
 }
