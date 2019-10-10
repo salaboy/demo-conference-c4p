106d3cd8ba458322258ca259674d7cd7d76439d2
salaboy <salaboy@gmail.com>
2019-10-10 09:37:36 +0100

    adding call to Agenda

--- src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
@@ -1,12 +1,16 @@
 package com.salaboy.conferences.c4p;
 
+import com.salaboy.conferences.c4p.model.AgendaItem;
 import com.salaboy.conferences.c4p.model.Proposal;
 import com.salaboy.conferences.c4p.model.ProposalDecision;
 import com.salaboy.conferences.c4p.model.ProposalStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
@@ -18,8 +22,12 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    private static final String agendaService = "http://demo-conference-agenda";
+
     @Value("${version:0.0.0}")
     private String version;
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     private Set<Proposal> proposals = new HashSet<>();
 
@@ -30,7 +38,7 @@ public class DemoApplication {
 
     @PostMapping()
     public String newProposal(@RequestBody Proposal proposal) {
-        System.out.println("> New Proposal Received: " + proposal);
+        System.out.println("> New Proposal Received Event ( " + proposal + ")");
         proposals.add(proposal);
         return "Thanks for submitting a Proposal, the conference organizers will get in touch soon";
     }
@@ -53,15 +61,17 @@ public class DemoApplication {
             Proposal proposal = proposalOptional.get();
             proposal.setApproved(decision.isApproved());
             proposal.setStatus(ProposalStatus.DECIDED);
-            emitEvent(">  Proposal Approved: " + ((decision.isApproved()) ? "Approved" : "Rejected"));
+            emitEvent("> Proposal Approved Event ( " + ((decision.isApproved()) ? "Approved" : "Rejected") + ")");
             proposals.add(proposal);
-            emitEvent("> Notify Speaker via email: " + proposal.getEmail() + " -> " + ((decision.isApproved()) ? "Approved" : "Rejected"));
+            emitEvent("> Notify Speaker Event (via email: " + proposal.getEmail() + " -> " + ((decision.isApproved()) ? "Approved" : "Rejected") + ")");
+            if (decision.isApproved()) {
+                emitEvent("> Add Proposal To Agenda Event ");
+                HttpEntity<AgendaItem> request = new HttpEntity<>(new AgendaItem(proposal.getTitle(), proposal.getAuthor(), new Date()));
+
+                restTemplate.postForEntity(agendaService, request, AgendaItem.class);
+            }
         }
 
-
-        if (decision.isApproved()) {
-            emitEvent("> Send request to publish talk to Agenda Service");
-        }
 
     }
 
