5c78c99697f81cafb784af620660812caf5a19db
salaboy <salaboy@gmail.com>
2019-10-13 10:49:30 +0100

    adding zeebe orchestration

--- src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
+++ src/main/java/com/salaboy/conferences/c4p/DemoApplication.java
@@ -4,6 +4,11 @@ import com.salaboy.conferences.c4p.model.AgendaItem;
 import com.salaboy.conferences.c4p.model.Proposal;
 import com.salaboy.conferences.c4p.model.ProposalDecision;
 import com.salaboy.conferences.c4p.model.ProposalStatus;
+import io.zeebe.client.api.response.WorkflowInstanceEvent;
+import io.zeebe.spring.client.EnableZeebeClient;
+import io.zeebe.spring.client.ZeebeClientLifecycle;
+import io.zeebe.spring.client.annotation.ZeebeDeployment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,15 +20,17 @@ import java.util.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+@EnableZeebeClient
+@ZeebeDeployment(classPathResource = "c4p-orchestration.bpmn")
 
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    private static final String AGENDA_SERVICE = "http://demo-conference-agenda";
-    private static final String EMAIL_SERVICE = "http://demo-conference-email";
+    @Autowired
+    private ZeebeClientLifecycle client;
 
     @Value("${version:0.0.0}")
     private String version;
@@ -40,6 +47,13 @@ public class DemoApplication {
     @PostMapping()
     public void newProposal(@RequestBody Proposal proposal) {
         proposals.add(proposal);
+        WorkflowInstanceEvent instance = client.newCreateInstanceCommand()
+                .bpmnProcessId("C4P")
+                .latestVersion()
+                .variables(Collections.singletonMap("proposal", proposal))
+                .send()
+                .join();
+
         emitEvent("> New Proposal Received Event ( " + proposal + ")");
     }
 
@@ -57,32 +71,41 @@ public class DemoApplication {
     public void rank(@PathVariable("id") String id, @RequestBody ProposalDecision decision) {
         emitEvent("> Proposal Approved Event ( " + ((decision.isApproved()) ? "Approved" : "Rejected") + ")");
         Optional<Proposal> proposalOptional = proposals.stream().filter(p -> p.getId().equals(id)).findFirst();
-
         if (proposalOptional.isPresent()) {
             Proposal proposal = proposalOptional.get();
-            // Apply Decision to Proposal
 
+            // Apply Decision to Proposal
             proposal.setApproved(decision.isApproved());
             proposal.setStatus(ProposalStatus.DECIDED);
             proposals.add(proposal);
+            client.newPublishMessageCommand().messageName("DecisionMade").correlationKey(proposal.getId())
+                    .variables(Collections.singletonMap("proposal", proposal)).send().join();
 
-            // Notify Potential Speaker By Email
-            HttpEntity<Proposal> requestEmail = new HttpEntity<>(proposal);
-            restTemplate.postForEntity(EMAIL_SERVICE, requestEmail, String.class);
-            emitEvent("> Notify Speaker Event (via email: " + proposal.getEmail() + " -> " + ((decision.isApproved()) ? "Approved" : "Rejected") + ")");
-
-            // Only if it is Approved create a new Agenda Item into the Agenda Service
-            if (decision.isApproved()) {
-                emitEvent("> Add Proposal To Agenda Event ");
-                HttpEntity<AgendaItem> requestAgenda = new HttpEntity<>(new AgendaItem(proposal.getTitle(), proposal.getAuthor(), new Date()));
-                restTemplate.postForEntity(AGENDA_SERVICE, requestAgenda, String.class);
-            }
+//            // Notify Potential Speaker By Email
+//            notifySpeakerByEmail(decision, proposal);
+//
+//            // Only if it is Approved create a new Agenda Item into the Agenda Service
+//            if (decision.isApproved()) {
+//                createAgendaItem(proposal);
+//            }
         } else {
             emitEvent(" Proposal Not Found Event (" + id + ")");
         }
 
 
     }
+
+//    private void createAgendaItem(Proposal proposal) {
+//        emitEvent("> Add Proposal To Agenda Event ");
+//        HttpEntity<AgendaItem> requestAgenda = new HttpEntity<>(new AgendaItem(proposal.getTitle(), proposal.getAuthor(), new Date()));
+//        restTemplate.postForEntity(AGENDA_SERVICE, requestAgenda, String.class);
+//    }
+//
+//    private void notifySpeakerByEmail(@RequestBody ProposalDecision decision, Proposal proposal) {
+//        emitEvent("> Notify Speaker Event (via email: " + proposal.getEmail() + " -> " + ((decision.isApproved()) ? "Approved" : "Rejected") + ")");
+//        HttpEntity<Proposal> requestEmail = new HttpEntity<>(proposal);
+//        restTemplate.postForEntity(EMAIL_SERVICE, requestEmail, String.class);
+//    }
 
     private void emitEvent(String content) {
         System.out.println(content);
